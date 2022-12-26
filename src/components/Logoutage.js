7bc5473cf627a73d8bf45f5cd9fe16257dd48700
JeevarehaR <jeev.reh@gmail.com>
2022-12-26 19:12:27 +0530

    commited

--- src/components/Logoutage.js
+++ src/components/Logoutage.js
@@ -1,31 +1,55 @@
-import React from 'react';
+import React from "react";
 
 // Logout display component
 export const Logout = () => {
-    return (
-        <div className="logout-container">
-            <a className="scroll-to-top rounded" href="#page-top">
-                <i className="fas fa-angle-up"></i>
-            </a>
+  return (
+    <div className="logout-container">
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+      </a>
 
-
-            <div className="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                            <button className="close" type="button" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                        <div className="modal-footer">
-                            <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                            <a className="btn btn-primary" data-dismiss="modal" href="!#">Logout</a>
-                        </div>
-                    </div>
-                </div>
+      <div
+        className="modal fade"
+        id="logoutModal"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Ready to Leave?
+              </h5>
+              <button
+                className="close"
+                type="button"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">×</span>
+              </button>
             </div>
+            <div className="modal-body">
+              Select "Logout" below if you are ready to end your current
+              session.
+            </div>
+            <div className="modal-footer">
+              <button
+                className="btn btn-secondary"
+                type="button"
+                data-dismiss="modal"
+              >
+                Cancel
+              </button>
+              <a className="btn btn-primary" data-dismiss="modal" href="!#">
+                Logout
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
